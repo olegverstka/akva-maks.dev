17aa83e268b697e2fec88ab5340fc6beeef88e89
olegverstka <olegverstka@gmail.com>
2016-01-30 12:13:03 +0300

    Сверстал страницу

--- slicing/app/js/main.js
+++ slicing/app/js/main.js
@@ -1,36 +1,49 @@
-var wrapper = $( ".file_upload" ),
-    inp = wrapper.find( "input" ),
-    btn = wrapper.find( "button" ),
-    lbl = wrapper.find( "div" );
-var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
-    
-    btn.focus(function(){
-        inp.focus()
-    });
-    // Crutches for the :focus style:
-    inp.focus(function(){
-        wrapper.addClass( "focus" );
-    }).blur(function(){
-        wrapper.removeClass( "focus" );
-    });
-
-    inp.change(function(){
-        var file_name;
-        if( file_api && inp[ 0 ].files[ 0 ] )
-            file_name = inp[ 0 ].files[ 0 ].name;
-        else
-            file_name = inp.val().replace( "C:\\fakepath\\", '' );
-
-        if( ! file_name.length )
-            return;
-
-        if( lbl.is( ":visible" ) ){
-            lbl.text( file_name );
-            btn.text( "Обзор" );
-        }else
-            btn.text( file_name );
-    }).change();
-
+var options = {
+  offset: 100
+}
+var header = new Headhesive('.navigation', options);
+$(window).load(function(){
+	$('.main-menu').liLanding();
+})
 $(document).ready(function(){
+	$('.bxslider').bxSlider({
+		pagerCustom : '.product-slider-pager',
+		controls    : false
+	});
 
+	$(".of_slider").bxSlider();
+	
+	$("select").selecter();
+
+	$('input[type="radio"], input[type="checkbox"]').styler();
+
+	$(".phone_mask").inputmask({"mask": "+7 (999) 999-9999"});
+
+	$(".order, .order-red, .btn").fancybox({
+		'scrolling' : 'no',
+		'titleShow' : false,
+		'onClosed'  : function() {
+			$(".order").hide();
+		}
+	});
+
+	$('.fancy').fancybox({
+		'padding' : '0'
+	});
+
+	$('.fancybox-media').attr('rel', 'media-gallery').fancybox({
+		padding    : '0',
+		openEffect : 'none',
+		closeEffect : 'none',
+		prevEffect : 'none',
+		nextEffect : 'none',
+		width: 940,
+		height: 528,
+
+		arrows : false,
+		helpers : {
+			media : {},
+			buttons : {}
+		}
+	});
 });
